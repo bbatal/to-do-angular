0a8d8906124ca366101ecfcfa080b5706fd02f87
bart batalinski <bartbatalinski@gmail.com>
2024-04-14 07:32:16 -0400

    routing, details, forms added

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housing-location';
 
@@ -6,13 +7,16 @@ import { HousingLocation } from '../housing-location';
   selector: 'app-housing-location',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    RouterLink,
+    RouterOutlet
   ],
   template: `
   <section class="listing">
     <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
     <h2 class="listing-heading">{{ housingLocation.name }}</h2>
     <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+    <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
   </section>
   `,
   styleUrl: './housing-location.component.css'
