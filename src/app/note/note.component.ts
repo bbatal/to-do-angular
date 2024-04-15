dbde9154bb83e3452e533b6f6a6f0a6b000b1d74
bart batalinski <bartbatalinski@gmail.com>
2024-04-15 13:34:29 -0400

    pseudocode for todo app and created component to generate a note

---
+++ src/app/note/note.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-note',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      note works!
+    </p>
+  `,
+  styleUrl: './note.component.css'
+})
+export class NoteComponent {
+
+}
